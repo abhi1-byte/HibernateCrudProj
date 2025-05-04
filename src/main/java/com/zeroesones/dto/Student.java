d72c0e1ca8625630450909cd61512d540a440120
Abhi <abhilashreddy571998@gmail.com>
2025-05-03 21:13:55 -0500

    Using JPA API primary key Generators

--- src/main/java/com/zeroesones/dto/Student.java
+++ src/main/java/com/zeroesones/dto/Student.java
@@ -1,61 +1,59 @@
 package com.zeroesones.dto;
 
-import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Student {
 
-	@Id
-	@GenericGenerator(name = "gen1",strategy = "increment")
-	@GeneratedValue(generator="gen1")
-	private Integer sid;
-	private String sname;
-	private String saddress;
-	private Integer sage;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer sid;
+    private String sname;
+    private String saddress;
+    private Integer sage;
 
-	public Student() {
-		System.out.println("Hibernate uses Zero argument constructor internally");
-	}
+    public Student() {
+        System.out.println("Hibernate uses Zero argument constructor internally");
+    }
 
-	public Integer getSid() {
-		return sid;
-	}
+    public Integer getSid() {
+        return sid;
+    }
 
-	public void setSid(Integer sid) {
-		this.sid = sid;
-	}
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
 
-	public String getSname() {
-		return sname;
-	}
+    public String getSname() {
+        return sname;
+    }
 
-	public void setSname(String sname) {
-		this.sname = sname;
-	}
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
 
-	public String getSaddress() {
-		return saddress;
-	}
+    public String getSaddress() {
+        return saddress;
+    }
 
-	public void setSaddress(String saddress) {
-		this.saddress = saddress;
-	}
+    public void setSaddress(String saddress) {
+        this.saddress = saddress;
+    }
 
-	public Integer getSage() {
-		return sage;
-	}
+    public Integer getSage() {
+        return sage;
+    }
 
-	public void setSage(Integer sage) {
-		this.sage = sage;
-	}
-	
-	@Override
-	public String toString() {
-		return "Student [sid=" + sid + ", sname=" + sname + ", saddress=" + saddress + ", sage=" + sage + "]";
-	}
+    public void setSage(Integer sage) {
+        this.sage = sage;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [sid=" + sid + ", sname=" + sname + ", saddress=" + saddress + ", sage=" + sage + "]";
+    }
 
 }
